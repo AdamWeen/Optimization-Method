3dbc25db56475e3d72a30c38a0d0a8e31f77a627
Adam Wen <wenxunzhi1@163.com>
2022-04-12 23:26:54 +0800

    画了几幅图，完善了一下

--- 作业/func_3_2.m
+++ 作业/func_3_2.m
@@ -5,6 +5,8 @@ function [output] = func_3_2(f,a0,b0,epsilon)
     x1=a0;x2=(a0+b0)/2;x3=b0;f1=f(x1);f2=f(x2);f3=f(x3);
     x=0.5*((x2^2-x3^2)*f1+(x3^2-x1^2)*f2+(x1^2-x2^2)*f3)/((x2-x3)*f1+ ...
     (x3-x1)*f2+(x1-x2)*f3);
+    %以下为画图考虑
+    k=1;kline=zeros(1,100,'int8');error = zeros(1,100,'double');
 while(abs(f(x2)-f(x))>=epsilon*f(x2))
     fx=f(x);f1=f(x1);f2=f(x2);f3=f(x3);
     if(x>x2)
@@ -22,12 +24,24 @@ while(abs(f(x2)-f(x))>=epsilon*f(x2))
     end
     x=0.5*((x2^2-x3^2)*f1+(x3^2-x1^2)*f2+(x1^2-x2^2)*f3)/((x2-x3)*f1+ ...
     (x3-x1)*f2+(x1-x2)*f3);
+    kline(k)=k;error(k) = abs(f(x2)-f(x));
     if(abs(f(x2)-f(x))<epsilon*f(x2))
         break;
     end
+    k=k+1;
 end
 if(f(x)<f(2))
     output = x;
 else
     output = x2;
 end
+kline(k)=k;error(k) = abs(f(x2)-f(x));
+%删除0值
+kline(kline==0)=[];
+error(error==0)=[];
+%绘图
+plot(kline,error,'-or');
+grid on;
+title("Distance between selected points");
+xlabel('Iterations');
+ylabel('Distance');
--- 作业/func_618.m
+++ 作业/func_618.m
@@ -4,8 +4,11 @@ function [output] = func_618(f,a0,b0,epsilon)
 lambda = [];mu = [];a = [];b=[];
 a=[a,a0];b=[b,b0];k=1;
 lambda =  [lambda,a0+0.382*(b0-a0)] ;mu(end+1) = a0+0.618*(b0-a0);
+%下一行为画图考虑。Sign用以标记输出哪个误差
+kline=zeros(1,100,'int8');error = zeros(1,100,'double');Sign = 0;
 while(b(k)-lambda(k)>epsilon&&mu(k)-a(k)>epsilon)
     if(f(lambda(k))>f(mu(k)))
+        Sign = 1;
         if(b(k)-lambda(k)<=epsilon)
             output = mu(k);
             break;
@@ -14,6 +17,7 @@ while(b(k)-lambda(k)>epsilon&&mu(k)-a(k)>epsilon)
         mu(k+1) = a(k+1)+0.618*(b(k+1)-a(k+1));
         end
     else
+        Sign = 2;
         if(mu(k)-a(k)<=epsilon)
             output = lambda(k);
             break;
@@ -22,10 +26,34 @@ while(b(k)-lambda(k)>epsilon&&mu(k)-a(k)>epsilon)
             lambda(k+1) = a(k+1)+0.382*(b(k+1)-a(k+1));
         end
     end
+    kline(k) = k;
+    if(Sign==1)
+        error(k) = b(k)-lambda(k);
+    elseif(Sign==2)
+        error(k) = mu(k)-a(k);
+    end
     k=k+1;
 end
+%输出结论
 if(b(k)-lambda(k)<=epsilon)
     output = mu(k);
 elseif(mu(k)-a(k)<=epsilon)
     output = lambda(k);
 end
+%加入最后一次迭代的信息
+kline(k) = k;
+if(Sign==1)
+    error(k) = b(k)-lambda(k);
+elseif(Sign==2)
+    error(k) = mu(k)-a(k);
+end
+%删除0值
+kline(kline==0)=[];
+error(error==0)=[];
+%绘图
+plot(kline,error,'-or');
+grid on;
+title("Distance between selected points");
+xlabel('Iterations');
+ylabel('Distance');
+
